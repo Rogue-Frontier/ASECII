4ddeb8e49ba2c82621edd3d0ed4a47fbf809bc38
Alex Chen <achen115@ucr.edu>
2020-04-22 00:03:21 -0700

    convert to module

--- ASECII/SpriteMenu.cs
+++ ASECII/SpriteMenu.cs
@@ -11,7 +11,7 @@ namespace ASECII {
         public EditorMain(int width, int height, Font font) :base(width, height, font) {
             UseKeyboard = true;
             UseMouse = true;
-            model = new SpriteModel();
+            model = new SpriteModel(32, 32);
             this.Children.Add(new SpriteMenu(32, 32, font, model) {
                 Position = new Point(8, 0),
                 FocusOnMouseClick = true
@@ -47,7 +47,7 @@ namespace ASECII {
             int hx = Width / 2;
             int hy = Height / 2;
 
-            var camera = model.camera - model.offsetPan;
+            var camera = model.camera - model.pan.offsetPan;
 
             for (int x = -hx; x < hx+1; x++) {
                 for(int y = -hy; y < hy+1; y++) {
@@ -68,94 +68,26 @@ namespace ASECII {
             base.Draw(timeElapsed);
         }
         public override bool ProcessKeyboard(Keyboard info) {
-            if (info.IsKeyDown(Space)) {
-                model.allowPan = true;
-            } else {
-                model.allowPan = false;
-            }
-
-            if (info.IsKeyPressed(Right)) {
-                model.cursor.X--;
-                model.keyboardMode = true;
-            }
-            if (info.IsKeyPressed(Left)) {
-                model.cursor.X++;
-                model.keyboardMode = true;
-            }
-            if (info.IsKeyPressed(Up)) {
-                model.cursor.Y++;
-                model.keyboardMode = true;
-            }
-            if (info.IsKeyPressed(Down)) {
-                model.cursor.Y--;
-                model.keyboardMode = true;
-            }
-            if (info.KeysPressed.Count == 1) {
-                char c = info.KeysPressed[0].Character;
-                if (char.IsLetterOrDigit(c)) {
-                    model.sprite.layers[0][model.cursor] = new ColoredGlyph(c, model.foreground, model.background);
-                }
-            }
+            model.ProcessKeyboard(info);
             return base.ProcessKeyboard(info);
         }
         public override bool ProcessMouse(MouseConsoleState state) {
-            var pressedLeft = !model.prevLeft && state.Mouse.LeftButtonDown;
-
-            if (state.ConsolePixelPosition != model.prevPixel) {
-                model.keyboardMode = false;
-            }
-            model.prevPixel = state.ConsolePixelPosition;
-
-            if(!model.keyboardMode) {
-                model.cursor = new Point(Width / 2, Height / 2) - state.ConsoleCellPosition + model.camera;
-            }
-            
-
-            model.glyph = (char)((model.glyph + state.Mouse.ScrollWheelValueChange/120 + 255) % 255);
-
-
             IsFocused = true;
-            if (model.startPan != null) {
-                if (!state.Mouse.LeftButtonDown) {
-                    model.camera -= model.offsetPan;
-                    model.startPan = null;
-                    model.offsetPan = Point.Zero;
-                } else {
-                    model.offsetPan = (Point)model.startPan - state.ConsoleCellPosition;
-                }
-            } else if (state.Mouse.LeftButtonDown) {
-                if (model.startPan == null && model.allowPan) {
-                    model.startPan = state.ConsoleCellPosition;
-                } else {
-                    var prev = model.prevCell;
-                    var offset = (model.cursor - prev);
-                    var length = offset.Length();
-                    for(int i = 0; i < length; i++) {
-                        var p = prev + new Point((int)(i * offset.X / length), (int)(i * offset.Y / length));
-                        if (model.sprite.InBounds(p)) {
-                            model.sprite.layers[0][p] = model.cg;
-                        }
-                    }
-
-                    if (model.sprite.InBounds(model.cursor)) {
-                        model.sprite.layers[0][model.cursor] = model.cg;
-                    }
-                }
-            }
-            model.prevCell = model.cursor;
-            model.prevLeft = state.Mouse.LeftButtonDown;
-
-            Done:
+            model.ProcessMouse(state);
             return base.ProcessMouse(state);
         }
     }
     class SpriteModel {
+        int width, height;
+
         public char glyph = 'A';
         public Color foreground = Color.Red;
         public Color background = Color.Black;
         public ColoredGlyph cg => new ColoredGlyph(glyph, foreground, background);
 
         public Sprite sprite = new Sprite(16, 16);
+
+        public PanMode pan;
 
         public int ticks = 0;
 
@@ -164,12 +96,110 @@ namespace ASECII {
         public Point prevPixel = new Point();
         public bool keyboardMode;
 
+        public Point prevCell;
+        public bool prevLeft;
+
+        public SpriteModel(int width, int height) {
+            this.width = width;
+            this.height = height;
+            pan = new PanMode(this);
+        }
+        public void ProcessKeyboard(Keyboard info) {
+            if (info.IsKeyDown(Space)) {
+                pan.allowPan = true;
+            } else {
+                pan.allowPan = false;
+            }
+
+            if (info.IsKeyPressed(Right)) {
+                cursor.X--;
+                keyboardMode = true;
+            }
+            if (info.IsKeyPressed(Left)) {
+                cursor.X++;
+                keyboardMode = true;
+            }
+            if (info.IsKeyPressed(Up)) {
+                cursor.Y++;
+                keyboardMode = true;
+            }
+            if (info.IsKeyPressed(Down)) {
+                cursor.Y--;
+                keyboardMode = true;
+            }
+            if (info.KeysPressed.Count == 1) {
+                char c = info.KeysPressed[0].Character;
+                if (char.IsLetterOrDigit(c)) {
+                    sprite.layers[0][cursor] = new ColoredGlyph(c, foreground, background);
+                }
+            }
+        }
+        public void ProcessMouse(MouseConsoleState state) {
+            var pressedLeft = !prevLeft && state.Mouse.LeftButtonDown;
+
+            if (state.ConsolePixelPosition != prevPixel) {
+                keyboardMode = false;
+            }
+            prevPixel = state.ConsolePixelPosition;
+
+            if (!keyboardMode) {
+                cursor = new Point(width / 2, height / 2) - state.ConsoleCellPosition + camera;
+            }
+
+
+            glyph = (char)((glyph + state.Mouse.ScrollWheelValueChange / 120 + 255) % 255);
+
+            if(!pan.ProcessMouse(state)) {
+                if(state.Mouse.LeftButtonDown) {
+                    var prev = prevCell;
+                    var offset = (cursor - prev);
+                    var length = offset.Length();
+                    for (int i = 0; i < length; i++) {
+                        var p = prev + new Point((int)(i * offset.X / length), (int)(i * offset.Y / length));
+                        if (sprite.InBounds(p)) {
+                            sprite.layers[0][p] = cg;
+                        }
+                    }
+
+                    if (sprite.InBounds(cursor)) {
+                        sprite.layers[0][cursor] = cg;
+                    }
+                }
+            }
+
+
+            prevCell = cursor;
+            prevLeft = state.Mouse.LeftButtonDown;
+        }
+    }
+    class PanMode {
+        SpriteModel model;
+
         public bool allowPan;
         public Point? startPan;     //Position on screen where user held down space and left clicked
         public Point offsetPan;
 
-        public Point prevCell;
-        public bool prevLeft;
+        public PanMode(SpriteModel model) {
+            this.model = model;
+        }
+        public bool ProcessMouse(MouseConsoleState state) {
+            if (startPan != null) {
+                if (!state.Mouse.LeftButtonDown) {
+                    model.camera -= offsetPan;
+                    startPan = null;
+                    offsetPan = Point.Zero;
+                } else {
+                    offsetPan = (Point)startPan - state.ConsoleCellPosition;
+                }
+                return true;
+            } else if (state.Mouse.LeftButtonDown) {
+                if (startPan == null && allowPan) {
+                    startPan = state.ConsoleCellPosition;
+                    return true;
+                }
+            }
+            return false;
+        }
     }
     class PaletteMenu : SadConsole.Console {
         SpriteModel spriteModel;
